cfa45eaedab4b76e26d95efd70f48b5b5a062de6
salmansheri <sheriffsalman00@gmail.com>
2025-04-06 18:26:13 +0530

    Search function

--- SearchService/Program.cs
+++ SearchService/Program.cs
@@ -8,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddHttp
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
---
+++ SearchService/Services/AuctionServiceHttpClient.cs
@@ -0,0 +1,30 @@
+using System;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Services;
+
+public class AuctionServiceHttpClient
+{
+
+    private readonly HttpClient _httpClient;
+    private readonly IConfiguration _config;
+    public AuctionServiceHttpClient(HttpClient httpClient, IConfiguration config)
+    {
+
+        _httpClient = httpClient;
+        _config = config;
+
+    }
+
+    public async Task<List<Item>> GetItemsForSearchDb()
+    {
+        var lastUpdated = await DB.Find<Item, string>()
+            .Sort(x => x.Descending(i => i.UpdatedAt))
+            .Project(x => x.UpdatedAt.ToString())
+            .ExecuteFirstAsync();
+
+        return await _httpClient.GetFromJsonAsync<List<Item>>(_config["AuctionServiceUrl"] + "/api/auction?date=" + lastUpdated);
+    }
+
+}
