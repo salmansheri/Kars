64149ef547efe7cfc0013cce29bf67e7121bbb8a
salmansheri <sheriffsalman00@gmail.com>
2025-06-15 08:32:53 +0530

    Dockerized SearchService

--- SearchService/Program.cs
+++ SearchService/Program.cs
@@ -18,9 +18,9 @@ builder.Services.AddMassTransit(options => {
 
     options.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false)); 
     options.UsingRabbitMq((context, cfg) => {
-        cfg.Host("localhost", "/", h=> {
-            h.Username("guest");
-            h.Password("guest");
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host => {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest")); 
+              host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest")); 
         }); 
         cfg.ReceiveEndpoint("search-auction-created", e=> {
             e.UseMessageRetry(r => r.Interval(5, 5)); 
